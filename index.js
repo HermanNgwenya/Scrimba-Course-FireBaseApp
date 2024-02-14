428c1f7114b0f553f4b9446847d5e2b10d9a6ce5
Herman Botshelo Ngwenya <hermanbngwenya@gmail.com>
2024-02-14 16:55:12 +0200

    javascript update

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+import { initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+
+const appSettings = {
+    databaseURL : "https://playground-b96f3-default-rtdb.firebaseio.com/"
+}
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
-
+ 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
 
-    console.log(inputValue)
+    console.log(`${inputValue} added to database`)
 })
