19ce8f08ffa7e8c9f3ec2decfa0238af2a161600
Herman Botshelo Ngwenya <hermanbngwenya@gmail.com>
2024-02-29 13:27:35 +0200

    added functionality

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-import { initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 
 const appSettings = {
-    databaseURL : "https://playground-b96f3-default-rtdb.firebaseio.com/"
+    databaseURL : "https://realtime-database-6745d-default-rtdb.firebaseio.com/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
  
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
 
+    push(shoppingListInDB, inputValue)
+
+    inputFieldEl.value = ""
+
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
     console.log(`${inputValue} added to database`)
 })
